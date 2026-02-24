ceb152ead70cc59f8ad9729c2969a4c77d8a4cbb
Richa Kandoi <richakandoi@richas-laptop.mynetworksettings.com>
2026-02-24 16:18:53 -0500

    added analytics sensor

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
+// This is the new line that imports the analytics tool
+import { Analytics } from '@vercel/analytics/react';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+    {/* This is the tag that actually tracks the visits */}
+    <Analytics />
+  </React.StrictMode>,
 )
